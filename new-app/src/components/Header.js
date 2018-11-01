291a0e3da81a412a6286ddf80a9f511f2afb59b8
aswallace <aswallace98@gmail.com>
2018-10-31 20:47:29 -0700

    Added basic router

---
+++ new-app/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+// The Header creates links that can be used to navigate
+// between routes.
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/appointment">Appointment</Link>
+        </li>
+        <li>
+          <Link to="/checkin">Check in</Link>
+        </li>
+        <li>
+          <Link to="/feedback">Feedback</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
--- new-app/src/index.js
+++ new-app/src/index.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from "react-dom";
-import Feedback from "./feedback";
-import CheckIn from "./checkin";
-import Appointment from "./appointment";
+// import Feedback from "./components/feedback";
+// import CheckIn from "./components/checkin";
+// import Appointment from "./components/appointment";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
 
 import "./styles.css";
 import * as serviceWorker from './serviceWorker';
 
-const View = () => (
-    <div>
-      <header>
-        <h1> MLRC </h1>
-      </header>
-      <Appointment />
-    </div>
-  );
+// const View = () => (
+//     <div>
+//       <header>
+//         <h1> MLRC </h1>
+//       </header>
+//       <Appointment />
+//     </div>
+//   );
   
-  render(<View />, document.getElementById("root"));
+//   render(<View />, document.getElementById("root"));
   
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
