21189aed8a5af81284f11d09d740675ffcc55357
aswallace <aswallace98@gmail.com>
2018-10-31 22:57:38 -0700

    Changed links to tabs

--- new-app/src/components/Header.js
+++ new-app/src/components/Header.js
@@ -1,5 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./headerstyle.css";
+// import { Route, Switch, Redirect } from 'react-router-dom';
+// import { RoutedTabs, NavTab } from 'react-router-tabs';
+// //import { Appointment, CheckIn, Feedback } from './components';
+ 
+// // with default styles:
+// import 'react-router-tabs/styles/react-router-tabs.css';
+// import Appointment from "./appointment";
+// import CheckIn from "./checkin";
+// import Feedback from "./feedback";
 
 // The Header creates links that can be used to navigate
 // between routes.
@@ -24,4 +34,21 @@ const Header = () => (
   </header>
 );
 
+ 
+// const Header = ({ match }) => {
+//   return (
+//     <div>
+//       <NavTab to="/appointment">Appointment</NavTab>
+//       <NavTab to="/checkin">Check in</NavTab>
+//       <NavTab to="/feedback">Feedback</NavTab>
+ 
+//       <Switch>
+//         <Route exact path={`${match.path}`} render={() => <Redirect replace to={`${match.path}/admins`} />} />
+//         <Route path={`${match.path}/appointment`} component={Appointment} />
+//         <Route path={`${match.path}/checkin`} component={CheckIn} />
+//         <Route path={`${match.path}/feedback`} component={Feedback} />
+//       </Switch>
+//     </div>
+//   );
+// };
 export default Header;
