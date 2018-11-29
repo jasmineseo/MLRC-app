1a006ad6347886b1158e49a725b3208bc554da46
aswallace <aswallace98@gmail.com>
2018-11-29 00:22:05 -0800

    Does fetch languages, but having trouble with arrays vs object types

--- my-app/src/components/CheckIn.js
+++ my-app/src/components/CheckIn.js
@@ -56,6 +56,33 @@ class CheckIn extends React.Component {
   }
 
   render() {
+    let langArray = [];
+    let newObject = []; // not sure why these are objects instead of arrays
+    alert("original type : " + typeof newObject);
+    var ref = firebase.database().ref('/language');
+    newObject = ref.once('value', function(snap) {
+      snap.forEach(function(childSnapshot) {
+        var item = childSnapshot.val();
+        item.key = childSnapshot.key;
+        langArray.push(item.key);
+      });
+      alert("languages " + langArray); // this seems correct?
+      return langArray
+    });
+    alert("langarray " + newObject);
+    alert("type of object: " + typeof newObject);
+    let array = Object.keys(newObject).map(i => newObject[i]);
+    alert("type of array: " + typeof array) // This should be an array, but it's currently an object...
+
+    // var Languages = ['Select a Language', 'Arabic', 'French', 'German', 
+                    // 'Italian', 'Korean', 'Spanish', 'Other'], // This works but is hard coded which we don't want
+    // var Languages = newObject, //doing it this way doesn't work bc says Languages.map is not a function
+    var Languages = Object.keys(newObject),
+    MakeItem = function(X) {
+      return <option>{X}</option>;
+    };
+    alert("Languages " + Languages) // this is empty...
+    
     return (
       <form id="checkInForm" onSubmit={this.handleSubmit}>
         <header>
@@ -161,14 +188,7 @@ class CheckIn extends React.Component {
             placeholder={"Select a language"}
             onChange={this.handleChange}
           >
-            <option value="Select a language">Select a language</option>
-            <option value="Arabic">Arabic</option>
-            <option value="French">French</option>
-            <option value="German">German</option>
-            <option value="Italian">Italian</option>
-            <option value="Korean">Korean</option>
-            <option value="Spanish">Spanish</option>
-            <option value="Other">Other</option>
+            {Languages.map(MakeItem)}
           </select>
         </label>
         <br />
