5d20dab340e0a64537ebcee9c7db57618e200348
Jasmine-Seo <jasmine-seo@Jasmines-MacBook.local>
2018-12-08 20:49:29 -0800

    feedback table works

--- my-app/src/components/DisplayFeedback.js
+++ my-app/src/components/DisplayFeedback.js
@@ -4,14 +4,9 @@ import ReactTable from 'react-table';
 import "react-table/react-table.css";
 import {firebase} from './firebase';
 import "./react-table.css";
-// import "./styles.css";
-
-
 
 
 class DisplayFeedback extends React.Component {
-  
-
   getFeedback(){
     var dicts = [];
     var ref = firebase.database().ref("feedback");
@@ -29,63 +24,16 @@ class DisplayFeedback extends React.Component {
     return dicts;
   }
 
-
-
-  // constructor(){
-  //   super();
-  //   this.state = {
-  //     data: []
-  //   };
-  //   this.fetchData = this.fetchData.bind(this);
-  // }
-
-  // fetchData(){
-  //   this.setState({
-  //     data: this.getFeedback()
-      
-  //   });
-  //   // const feedback = this.getFeedback();
-  //   // this.state.data = this.setState({ data: feedback});
-  //   // console.log(this.state.data);
-  // }
-
   render() {
+    const feedbackData = this.getFeedback();
 
-    
-    const feedbackdata = this.getFeedback();
-    console.log("data in render:", feedbackdata);
-    console.log("feedback type: ", typeof feedbackdata);
-    console.log("feedback element type: ", typeof feedbackdata[0]);
-    const data = [{
-      language: 'Spanish',
-      question1: "Homework",
-      question2: "My tutor could not answer my questions.",
-      question3: "No"
-      },{
-        language: 'French',
-        question1: "I reviewed French vocabulary words for an upcoming test for French 44",
-        question2: "My tutor, Sarah, was extremely helpful and friendly.",
-        question3: "I will defintely visit the MLRC again."
-      },{
-        language: 'Arabic',
-        question1: "The tutor reviewed my Arabic presentation that I have give for my Arabic class.",
-        question2: "I forgot her name.",
-        question3: "Maybe"
-      }, {
-        language: 'French',
-        question1: "I worked on my essay for French 33.",
-        question2: "The French tutor, Julia, helped me translate some words and corrected my grammatical errors. She is very considerate and patient even when I didn't understand some of her explanations.",
-        question3: "Yes! I would come again."
-      }];
-
-    // const feedbackdata = this.state;
     return (
       <div>
         <header>
           <h2> View student feedback </h2>
         </header>
         <ReactTable
-          data={feedbackdata}
+          data={feedbackData}
           minRows={0}
           columns={[
             {
